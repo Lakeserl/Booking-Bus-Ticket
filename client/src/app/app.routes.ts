cfbfdd508f776f303a2e3746c3cdce23af2fedc9
Lakeserl <dinhlam18112003@gmail.com>
2025-05-30 23:19:38 +0700

    Updated: Installing program

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { SearchComponent } from './page/search/search.component';
+import { SearchResultComponent } from './page/search-result/search-result.component';
+import { BookTicketComponent } from './page/book-ticket/book-ticket.component';
+import { MyBookingComponent } from './page/my-booking/my-booking.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        redirectTo: "search",
+        pathMatch: "full"
+    },
+    {
+        path:"search",
+        component:SearchComponent
+    },
+    {
+        path:"search-result",
+        component:SearchResultComponent
+    },
+    {
+        path:"book-ticket",
+        component:BookTicketComponent
+    },
+        {
+        path:"my-booking",
+        component:MyBookingComponent
+    },
+];
